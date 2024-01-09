c294cc21b1168446662ff8dabb61031c32106946
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-09 15:27:36 +0100

    fix pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """This module defines the function canUnlockAll"""
 
+
 def canUnlockAll(boxes):
     """This function takes a list of lists as argument
-        and every list presents a box containing keys that can open other boxes
-        and it returns true if all the boxes can be opened and false otherwise"""
-    
+        and every list presents a box containing keys
+        that can open other boxes
+        and it returns true if all the boxes
+        can be opened and false otherwise"""
     keys = [0]
     canOpen = True
     for box in boxes:
