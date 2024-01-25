4dedf4b9dda54d903b22693d640e7460922a6004
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-25 15:25:02 +0100

    fixing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,6 +6,17 @@ it prints File size: <total size>
 <status code>: <number> for every status code"""
 
 from sys import stdin
+import re
+
+
+def valid_format(line):
+    """checks if the line have a valid format"""
+    pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+    r'\[([^\]]+)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
+    match = re.match(pattern, line)
+    if match:
+        return True
+    return False
 
 
 try:
@@ -13,6 +24,8 @@ try:
     total_size = 0
     for i, line in enumerate(stdin, start=1):
         line = line.strip()
+        if not valid_format(line):
+            continue
         parts = line.split(" ")
         total_size += int(parts[-1])
         if parts[-2] not in my_dict:
