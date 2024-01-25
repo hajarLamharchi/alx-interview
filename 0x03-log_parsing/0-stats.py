194c5ad5674c0098fc128ae991c4655db55caa9a
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-25 22:36:10 +0100

    still trying

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -19,31 +19,30 @@ def valid_format(line):
     return False
 
 
-if __name__ == '__main__':
-    try:
-        my_dict = {}
-        total_size = 0
-        for i, line in enumerate(stdin, start=1):
-            line = line.strip()
-            if not valid_format(line):
-                continue
-            parts = line.split()
-            try:
-                total_size += int(parts[-1])
-                if parts[-2] not in my_dict:
-                    my_dict[parts[-2]] = 1
-                else:
-                    my_dict[parts[-2]] += 1
-                my_dict = dict(sorted(my_dict.items()))
-            except BaseException:
-                continue
-            if i % 10 == 0:
-                print("File size: {}".format(total_size))
-                for key, val in my_dict.items():
-                    print("{}: {}".format(key, val))
-    except Exception as err:
-        pass
-    finally:
-        print("File size: {}".format(total_size))
-        for key, val in my_dict.items():
-            print("{}: {}".format(key, val))
+try:
+    my_dict = {}
+    total_size = 0
+    for i, line in enumerate(stdin, start=1):
+        line = line.strip()
+        if not valid_format(line):
+            continue
+        parts = line.split()
+        try:
+            total_size += int(parts[-1])
+            if parts[-2] not in my_dict:
+                my_dict[parts[-2]] = 1
+            else:
+                my_dict[parts[-2]] += 1
+            my_dict = dict(sorted(my_dict.items()))
+        except BaseException:
+            continue
+        if i % 10 == 0:
+            print("File size: {}".format(total_size))
+            for key, val in my_dict.items():
+                print("{}: {}".format(key, val))
+except Exception as err:
+    pass
+finally:
+    print("File size: {}".format(total_size))
+    for key, val in my_dict.items():
+        print("{}: {}".format(key, val))
