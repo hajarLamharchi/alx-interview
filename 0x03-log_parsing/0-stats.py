6d4e8b9da9cd0b8ed8f76fac2ba7c5ad1cbb92e4
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-25 22:36:53 +0100

    still trying

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -27,15 +27,12 @@ try:
         if not valid_format(line):
             continue
         parts = line.split()
-        try:
-            total_size += int(parts[-1])
-            if parts[-2] not in my_dict:
-                my_dict[parts[-2]] = 1
-            else:
-                my_dict[parts[-2]] += 1
-            my_dict = dict(sorted(my_dict.items()))
-        except BaseException:
-            continue
+        total_size += int(parts[-1])
+        if parts[-2] not in my_dict:
+            my_dict[parts[-2]] = 1
+        else:
+            my_dict[parts[-2]] += 1
+        my_dict = dict(sorted(my_dict.items()))
         if i % 10 == 0:
             print("File size: {}".format(total_size))
             for key, val in my_dict.items():
