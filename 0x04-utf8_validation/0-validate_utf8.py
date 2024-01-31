a5ce48cb89f52bf20a70861bb0f000dda5adb6b6
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-31 21:53:39 +0100

    0x04-utf8_validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this script defines the function validUTF8"""
+
+
+def validUTF8(data):
+    """this function checks if data is utf-8 valid"""
+    is_valid = True
+    for c in data:
+        try:
+            b = c.to_bytes(8, byteorder='big')
+            b.decode('utf-8')
+            continue
+        except UnicodeDecodeError:
+            is_valid = False
+    return is_valid
