50bb0208146d50d3f7c8db5ff4bc1c2d784769fe
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-02-14 14:55:28 +0100

    using async and promise to get the characters in the correct order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,20 +4,25 @@ const request = require('request');
 
 const movieID = process.argv[2];
 const options = { json: true };
-const url = 'https://swapi-api.alx-tools.com/api/films/' + movieID;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-request(url, options, (error, response, body) => {
+request(url, options, async (error, response, body) => {
   if (error) {
     console.error(error);
+    return;
   }
 
   for (const character of body.characters) {
-    request(character, options, (error, response, body) => {
-      if (error) {
-        console.error(error);
-      }
+    await new Promise((resolve) => {
+      request(character, options, (error, response, body) => {
+        if (error) {
+          console.error(error);
+          resolve();
+        }
 
-      console.log(body.name);
+        console.log(body.name);
+        resolve();
+      });
     });
   }
 });
